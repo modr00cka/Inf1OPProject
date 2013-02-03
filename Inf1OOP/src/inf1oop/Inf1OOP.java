0ab3c02e2a260ac15ea3e4483b06d7d85c195668
M. <>
2013-02-03 21:59:45 +0000

    Strings

---
+++ Inf1OOP/src/inf1oop/Inf1OOP.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf1oop;
+
+/**
+ * Write a description of class Memorization here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mem
+{
+    String s;
+    String [] stringz;
+    int [][] numz;
+    public Mem (String s)
+    {
+        this.s=s;
+    }
+    
+    // gets how many characters C are in the string s - C works as a dividing character
+    
+    public static int counts(String s, char c)
+    {
+        int count = 0;
+        for(int i =0; i<s.length();i++)
+        {
+            if(s.substring(i,i+1).equals((""+c)))count++;
+        }
+        return count+1;
+    }
+    
+    
+    public void split( char c)
+    {
+        String [] stringz = new String [counts(s,c)];
+        int u=0;
+        String x="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.substring(i,i+1).equals(""+c) )
+            {
+                stringz [u]=x;
+                u++;
+                x="";
+            }
+            else if(s.length()-1==i)
+            {
+                stringz [u]=x+s.substring (i,i+1);
+                u++;
+            }
+            else
+            {
+                x=x+s.substring(i,i+1);
+            }
+        }
+        
+        for(int i=0;i<u;i++)
+        {
+            System.out.println(stringz[i]);
+        }
+        this.stringz=stringz;
+    }
+    
+    public void splitnums(char c, int o)
+    {
+        String s=stringz[o];
+        String s1="";
+        String s2="";
+        int i=0;
+        while(!s.substring(i,i+1).equals(c+""))
+        {
+            i++;
+        }
+        s1=s.substring(0,i);
+        s2=s.substring(i+1,s.length());
+        int n1=Integer.parseInt(s1);
+        int n2=Integer.parseInt(s2);
+        numz[o][0]=n1;
+        numz[o][1]=n2;
+    }
+    
+    public void memgame(char c)
+    {
+        numz=new int [stringz.length][2]; 
+        for(int i=0; i<stringz.length;i++)
+        {
+            splitnums(c,i);
+        }
+    }
+    
+    public void print()
+    {
+        for(int i=0; i<numz.length;i++)
+        {
+            System.out.println(numz[i][0] + " , " + numz[i][1] );
+        }
+    }
+    
+    public static void main()
+    {
+        Mem m= new Mem("12,13;15,16;11,15;19,19");
+        m.split(';');
+        m.memgame(',');
+        m.print();
+    }
+}
